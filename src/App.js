00ffd0bd1f069fa28b56d8dceed5b4c3899c72b9
Imran Hadid <imranhadid03@gmail.com>
2020-05-18 16:38:53 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/index';
-import Movies from './components/Movies/index';
+// import Header from './components/Header/index';
+// import Movies from './components/Movies/index';
+import AllInOne from './components/AllInOne';
 
 function App() {
     return (
         <div className="App">
-            <Header />
-            <Movies />
+            {/* <Header />*/}
+            {/* <Movies /> */}
+            <AllInOne />
         </div>
     );
 }
